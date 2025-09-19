aeaa5357956a8a984e361ee60b18b6838b87edf9
hughfenghen <hughfenghen@gmail.com>
2025-09-19 14:21:21 +0800

    自定义 tab 快捷键切换buffer

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -21,6 +21,90 @@ end
 
 -- vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true })
 -- vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true })
+-- Tab 显示 buffer 列表
+vim.keymap.set("n", "<Tab>", function()
+  Snacks.picker.buffers {
+    sort_lastused = true, -- 修正：使用正确的配置项名称
+    current = false, -- 修正：使用 current = false 来忽略当前缓冲区
+    prompt = "Switch Buffer",
+    win = {
+      input = {
+        keys = {
+          ["<Tab>"] = { "list_down", mode = { "n", "i" } },
+          ["<S-Tab>"] = { "list_up", mode = { "n", "i" } },
+        },
+      },
+    },
+    format = function(item, picker)
+      local ret = {}
+      -- Column 1: Buffer ID (Orange)
+      ret[#ret + 1] = { string.format("%-4s", tostring(item.buf)), "WarningMsg" }
+
+      local path = Snacks.picker.util.path(item) or item.file
+      path = Snacks.picker.util.truncpath(path, picker.opts.formatters.file.truncate or 40, { cwd = picker:cwd() })
+      local name, cat = path, "file"
+      if item.buf and vim.api.nvim_buf_is_loaded(item.buf) then
+        name = vim.bo[item.buf].filetype
+        cat = "filetype"
+      elseif item.dir then
+        cat = "directory"
+      end
+
+      if picker.opts.icons.files.enabled ~= false then
+        local icon, hl = Snacks.util.icon(name, cat, {
+          fallback = picker.opts.icons.files,
+        })
+        if item.dir and item.open then icon = picker.opts.icons.files.dir_open end
+        icon = Snacks.picker.util.align(icon, picker.opts.formatters.file.icon_width or 2)
+        ret[#ret + 1] = { icon, hl, virtual = true }
+      end
+
+      local filepath = item.file or ""
+      local filename = vim.fn.fnamemodify(filepath, ":t")
+
+      ret[#ret + 1] = { filename, "SnacksPickerFile" }
+
+      -- 尝试获取 git root 路径和项目信息
+      local git_root = ""
+      local project_name = ""
+
+      if item.info and item.info.variables and item.info.variables.gitsigns_status_dict then
+        git_root = item.info.variables.gitsigns_status_dict.root or ""
+        if git_root ~= "" then
+          -- 提取项目名称（git root 的最后一个目录名）
+          project_name = vim.fn.fnamemodify(git_root, ":t")
+        end
+      end
+
+      local display_path = ""
+
+      if git_root ~= "" then
+        -- 显示 项目名/相对路径
+        local relative_path = filepath:gsub("^" .. vim.pesc(git_root) .. "/", "")
+        local relative_dir = vim.fn.fnamemodify(relative_path, ":h")
+
+        if relative_dir == "." then
+          display_path = project_name
+        else
+          display_path = project_name .. "/" .. relative_dir
+        end
+      else
+        -- 没有 git root，显示相对路径
+        display_path = vim.fn.fnamemodify(filepath, ":~:.:h")
+        if display_path == "." then display_path = "" end
+      end
+
+      -- 设置文件名的固定宽度
+      if display_path ~= "" then
+        ret[#ret + 1] = { "    ", "None" } -- Separator
+        ret[#ret + 1] = { display_path, "SnacksPickerComment" }
+      end
+
+      return ret
+    end,
+  }
+end, { desc = "Switch buffers" })
+
 vim.keymap.set("n", "<Enter>", function()
   if vim.bo.modifiable and not vim.bo.readonly then
     feedkeys "o<Esc>"
