919cb18b5ccc3bddfc6e31d62991528ff943c27b
hughfenghen <hughfenghen@gmail.com>
2026-01-15 21:42:09 +0800

    update

--- lua/plugins/fenghen-im-select.lua
+++ lua/plugins/fenghen-im-select.lua
@@ -1,6 +1,7 @@
 return {
-  name = "fenghen-im-select",
+  -- name = "fenghen-im-select",
   dir = "/Users/fenghen/my-space/fenghen-im-select.nvim", -- 本地路径
+  -- "hughfenghen/fenghen-im-select.nvim",
   -- enabled = false,
   config = function()
     -- macOS 配置
